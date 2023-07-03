b6198dc9e34948ea2df36babddeb3a92a510b9e9
FilipeGPaganoto <123755494+FilipeGPaganoto@users.noreply.github.com>
2023-07-03 20:51:20 -0300

    Class001

---
+++ academy/devdojo/javaoneforall/introduction/Class001HelloDevDojo.java
@@ -0,0 +1,7 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class001HelloDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Ka-me-ha-me-ha!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    }
+}
